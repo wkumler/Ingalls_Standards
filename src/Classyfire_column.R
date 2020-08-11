e88fabfcbbd42857295c0214e51f031ad8fba807
Regina Lionheart <regina@lionhe.art>
2020-08-11 11:59:52 -0700

    Bug fix tsv -> csv

--- src/Classyfire_column.R
+++ src/Classyfire_column.R
@@ -11,13 +11,8 @@ Combined.Classyfire <- Classyfire %>%
   unite(Combined, c("Level", "Classification"), sep = ": ") %>%
   arrange(Compound.Name)
 
-## works only for one compound
-one.compound <- Combined.Classyfire %>%
-  filter(Compound.Name == "Adenine")
 
-outcome <- rbind(one.compound, row3 = apply(one.compound[2], 2, paste0, collapse = "; "))
-
-## Try for all of them
+## Create dataframe of 
 split.by.compound <- split(Combined.Classyfire, f = Combined.Classyfire$Compound.Name)
 
 New.rows <- lapply(split.by.compound, function(x) {
@@ -32,3 +27,5 @@ All.Classyfire <- bind_rows(New.rows)
 Ingalls_Lab_Standards_Classyfire <- Ingalls_Lab_Standards %>%
   left_join(All.Classyfire, by = "Compound.Name") %>%
   select(-1)
+
+rownames(Ingalls_Lab_Standards_Classyfire) <- NULL
